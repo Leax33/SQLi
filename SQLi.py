2154d52cc89439517d0d5a5fcdda14f70670646b
Tuhin Bose <62632413+tuhin1729@users.noreply.github.com>
2020-05-05 17:39:01 +0530

    Update SQLi.py

--- SQLi.py
+++ SQLi.py
@@ -38,6 +38,7 @@ banner()
 checkvpn()
 for url in search(q,tld="com",num=no,stop=no,pause=2):
  if("php?" not in url):
+  i=i+1
   continue   
  print("\033[1;37;40m"+str(i)+". \033[1;35;40mChecking in this URL: ")
  print("\033[1;34;40m"+url)
