045b44dc79f5621f762fcd8b0c43208563996928
Tuhin Bose <62632413+tuhin1729@users.noreply.github.com>
2020-05-06 00:47:09 +1400

    Update SQLi.py

--- SQLi.py
+++ SQLi.py
@@ -42,7 +42,7 @@ for url in search(q,tld="com",num=no,stop=no,pause=2):
  print("\033[1;37;40m"+str(i)+". \033[1;35;40mChecking in this URL: ")
  print("\033[1;34;40m"+url)
  try:
-  if(("af.org.pk" in url) or ("india.org.pk" in url)):
+  if(("af.org.pk" in url) or ("india.org.pk" in url) or ("na.gov.pk" in url)):
    print("\033[1;31;40mNot Vulnerable!\n")
    i=i+1
    continue
@@ -52,7 +52,7 @@ for url in search(q,tld="com",num=no,stop=no,pause=2):
   if(r.text==s.text):
    print("\033[1;31;40mNot Vulnerable!\n")
   else:
-   print("\033[1;32;40mVulnerable\n")
+   print("\033[1;32;40mProbably Vulnerable.\n")
    if(op=="Y" or op=="y"):
     f.write(url+"\n")   
  except:
